4f624aacd8acc8876280cf847122d608d4207f6d
imdavidmin <48481563+imdavidmin@users.noreply.github.com>
2022-09-12 21:03:20 +0100

    3 files modified

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('Hello!');
+async function submitReport() {
+  function getVal(id) {
+    return document.querySelector(`#${id}`).value;
+  }
+  const payload = {
+    type: getVal('issue'),
+    location: getVal('location'),
+    unit: getVal('unit'),
+    comment: getVal('comment'),
+  };
+  const r = await fetch('https://api.lansburysquare.com/report', {
+    method: 'POST',
+    headers: { 'content-type': 'application/json' },
+    body: JSON.stringify(payload),
+  });
+  if (r.status == 200) {
+    const button = document.querySelector('button');
+    button.disabled = true;
+    button.innerText = 'Submission Successful';
+  }
+}
